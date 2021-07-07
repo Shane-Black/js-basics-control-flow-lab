fe7901249e134d43801345a36f3e2f11166746b8
Shane <shane.black@hey.com>
2021-07-07 23:23:23 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet < 400)
+  return 'This one is on me!'
+  if (feet < 2500 && feet > 2000)
+  return 'I will gladly take your thirty bucks.'
+  if (feet >2500)
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case "not as generous":
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+
 }
+
